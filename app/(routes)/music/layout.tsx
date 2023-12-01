ec892a19f198ca6974471a6d6b135a23efa46094
nohr <aite@nyu.edu>
2023-12-01 02:50:29 -0600

    update mobile music layout

--- app/(routes)/music/layout.tsx
+++ app/(routes)/music/layout.tsx
@@ -7,9 +7,9 @@ export default function MusicLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="grid h-full w-full max-w-prose grid-cols-1 grid-rows-[0.5fr_3fr] items-start gap-1 md:gap-4">
-      <Player />
+    <div className="grid h-full w-full max-w-prose grid-cols-1 grid-rows-[3fr_0.5fr] items-start gap-1 md:grid-rows-[0.5fr_3fr] md:gap-4">
       <Explorer />
+      <Player />
       {children}
     </div>
   );
