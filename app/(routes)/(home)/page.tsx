41b1b007217efcd7a07cf737bc3d5b80f9c8cadf
nohr <aite@nyu.edu>
2024-01-17 00:52:12 -0600

    don't cache first page

--- app/(routes)/(home)/page.tsx
+++ app/(routes)/(home)/page.tsx
@@ -3,8 +3,10 @@ import Name from "./Name";
 import Locale from "./Locale";
 import { PortableText } from "@portabletext/react";
 import Section from "./section";
+import { unstable_noStore } from "next/cache";
 
 export default async function Home() {
+  unstable_noStore();
   const { bio, _id, timeZone, location } = await getInfo();
 
   return (
